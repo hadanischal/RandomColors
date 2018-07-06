65a5035350fd0d1d5f3b135ee3380cb4698b97e5
Nischal Hada <hadanischal@gmail.com>
2018-07-06 21:42:12 +1000

    added ThemeColor constants

---
+++ RandomColors/ThemeColor/ThemeColor.swift
@@ -0,0 +1,13 @@
+//
+//  ThemeColor.swift
+//  RandomColors
+//
+//  Created by Nischal Hada on 7/6/18.
+//  Copyright © 2018 NischalHada. All rights reserved.
+//
+
+import Foundation
+
+enum ThemeColor {
+
+}
