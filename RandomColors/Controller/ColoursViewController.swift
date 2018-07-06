04b5dbfaafca0eb7ccb07216470e4decd67e550c
Nischal Hada <hadanischal@gmail.com>
2018-07-06 17:17:09 +1000

    setup UICollectionView

--- RandomColors/Controller/ColoursViewController.swift
+++ RandomColors/Controller/ColoursViewController.swift
@@ -9,27 +9,76 @@
 import UIKit
 
 class ColoursViewController: UIViewController {
-
+    @IBOutlet var collectionView: UICollectionView?
+    fileprivate let sectionInsets = UIEdgeInsets(top: 5.0, left: 5.0, bottom: 5.0, right: 5.0)
+    fileprivate let itemsPerRow: CGFloat = 3
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        self.setupUI()
+        self.setupCollectionView()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func setupUI() {
+        self.title = "Random Colours"
+        self.view.backgroundColor = UIColor.white
     }
-    */
+}
+
+// MARK: UICollectionViewDataSource
+extension ColoursViewController: UICollectionViewDataSource {
+    func setupCollectionView() -> Void{
+        guard let layout = self.collectionView?.collectionViewLayout as? UICollectionViewFlowLayout else {
+            return
+        }
+        layout.scrollDirection = UICollectionViewScrollDirection.vertical
+        self.collectionView?.collectionViewLayout = layout
+        layout.minimumLineSpacing = 0
+        layout.minimumInteritemSpacing = 0
+        self.collectionView?.backgroundColor = UIColor.white
+        self.collectionView?.showsHorizontalScrollIndicator = false
+    }
+ 
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ColoursViewCell", for: indexPath) as! ColoursViewCell
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("didSelect Item")
+    }
 
 }
+
+// MARK: UICollectionViewDelegateFlowLayout
+extension ColoursViewController : UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize{
+        let paddingSpace = sectionInsets.left * (itemsPerRow + 1)
+        let availableWidth = view.frame.width - paddingSpace
+        let widthPerItem = availableWidth / itemsPerRow
+        let heightPerItem = widthPerItem + 21
+        return CGSize(width: widthPerItem, height: heightPerItem)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets{
+        return sectionInsets
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat{
+        return sectionInsets.left
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat{
+        return 0
+    }
+}
+
---
+++ RandomColors/View/ColoursViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ColoursViewCell.swift
+//  RandomColors
+//
+//  Created by Nischal Hada on 7/6/18.
+//  Copyright © 2018 NischalHada. All rights reserved.
+//
+
+import UIKit
+
+class ColoursViewCell: UICollectionViewCell {
+    
+}
