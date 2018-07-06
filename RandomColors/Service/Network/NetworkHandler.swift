b603dbf5022032a59235bf1aecb269834ceee760
Nischal Hada <hadanischal@gmail.com>
2018-07-06 22:18:16 +1000

    implemented NetworkHandler

--- RandomColors/Service/Network/NetworkHandler.swift
+++ RandomColors/Service/Network/NetworkHandler.swift
@@ -6,8 +6,43 @@
 //  Copyright © 2018 NischalHada. All rights reserved.
 //
 
-import UIKit
+import Foundation
 
 class NetworkHandler {
-
+    let reachability = Reachability()!
+    func networkResult<T: Parceable>(completion: @escaping ((Result<[T], ErrorResult>) -> Void)) ->
+        ((Result<Data, ErrorResult>) -> Void) {
+            return { dataResult in
+                DispatchQueue.global(qos: .background).async(execute: {
+                    switch dataResult {
+                    case .success(let data) :
+                        ParserHelper.parse(data: data, completion: completion)
+                        break
+                    case .failure(let error) :
+                        print("Network error \(error)")
+                        completion(.failure(.network(string: "Network error " + error.localizedDescription)))
+                        break
+                    }
+                })
+            }
+    }
+    
+    func networkResult<T: Parceable>(completion: @escaping ((Result<T, ErrorResult>) -> Void)) ->
+        ((Result<Data, ErrorResult>) -> Void) {
+            return { dataResult in
+                DispatchQueue.global(qos: .background).async(execute: {
+                    switch dataResult {
+                    case .success(let data) :
+                        ParserHelper.parse(data: data, completion: completion)
+                        break
+                    case .failure(let error) :
+                        print("Network error \(error)")
+                        completion(.failure(.network(string: "Network error " + error.localizedDescription)))
+                        break
+                    }
+                })
+            }
+    }
 }
+
+
