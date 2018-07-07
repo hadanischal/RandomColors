606900cfdc3b8f12031073b1ae88800127b50ed7
Nischal Hada <hadanischal@gmail.com>
2018-07-07 16:44:09 +1000

    implementing ColoursViewModel

--- RandomColors/ViewModels/ColoursViewModel.swift
+++ RandomColors/ViewModels/ColoursViewModel.swift
@@ -7,3 +7,41 @@
 //
 
 import Foundation
+
+class ColoursViewModel {
+    // MARK: - Input
+    weak var dataSource : GenericDataSource<ColoursModel>?
+    
+    // MARK: - Output
+    weak var service: ColoursServiceCallProtocol?
+    var onErrorHandling : ((ErrorResult?) -> Void)?
+ 
+    init(service: ColoursServiceCallProtocol? = ColoursServiceCall.shared, dataSource : GenericDataSource<ColoursModel>?) {
+        self.dataSource = dataSource
+        self.service = service
+    }
+    
+    func fetchServiceCall(_ completion: ((Result<Bool, ErrorResult>) -> Void)? = nil) {
+        
+        guard let service = service else {
+            onErrorHandling?(ErrorResult.custom(string: "Missing service"))
+            return
+        }
+        service.fetchConverter() { result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let converter) :
+                    self.dataSource?.data.value = converter
+                    completion?(Result.success(true))
+                    break
+                case .failure(let error) :
+                    print("Parser error \(error)")
+                    self.onErrorHandling?(error)
+                    completion?(Result.failure(error))
+                    break
+                }
+            }
+        }
+    }
+ 
+}
