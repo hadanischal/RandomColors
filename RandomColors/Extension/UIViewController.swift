7603fe92bdc26ff305f45eccf14980d99f5a3ded
Nischal Hada <hadanischal@gmail.com>
2018-07-07 16:50:32 +1000

    implemented UIViewController extension

---
+++ RandomColors/Extension/UIViewController.swift
@@ -0,0 +1,19 @@
+//
+//  UIViewController.swift
+//  RandomColors
+//
+//  Created by Nischal Hada on 7/7/18.
+//  Copyright © 2018 NischalHada. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController{
+    // Returns the error message
+    func showAlert(title: String?, message: String?){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: { (action) -> Void in
+        }))
+        self.present(alert, animated: true, completion: nil)
+    }
+}
